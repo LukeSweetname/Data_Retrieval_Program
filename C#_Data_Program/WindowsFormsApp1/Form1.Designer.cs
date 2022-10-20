2d2ce68bda7fefd88c5c9a1654d6c699a3d477bc
LukeSweetname <2202448@uad.ac.uk>
2022-10-20 15:54:58 +0100

    Revised title to label

--- C#_Data_Program/WindowsFormsApp1/Form1.Designer.cs
+++ C#_Data_Program/WindowsFormsApp1/Form1.Designer.cs
@@ -31,7 +31,6 @@ namespace WindowsFormsApp1
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.systemNameInput = new System.Windows.Forms.Label();
             this.modelInput = new System.Windows.Forms.Label();
             this.manufacturerInput = new System.Windows.Forms.Label();
@@ -46,6 +45,7 @@ namespace WindowsFormsApp1
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -66,15 +66,6 @@ namespace WindowsFormsApp1
             this.button2.TabIndex = 1;
             this.button2.Text = "Check Hardware";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(198, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(425, 53);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "Data Retrieval Program";
             // 
             // systemNameInput
             // 
@@ -203,11 +194,22 @@ namespace WindowsFormsApp1
             this.label12.TabIndex = 25;
             this.label12.Text = "Extra Data - Header";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(177, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 46);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Data Retrieval Program";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
@@ -222,7 +224,6 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.manufacturerInput);
             this.Controls.Add(this.modelInput);
             this.Controls.Add(this.systemNameInput);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -236,7 +237,6 @@ namespace WindowsFormsApp1
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label systemNameInput;
         private System.Windows.Forms.Label modelInput;
         private System.Windows.Forms.Label manufacturerInput;
@@ -251,6 +251,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label1;
     }
 }
 
