4fac55773b6a6640d8df31845a725f2a478f2e5e
LukeSweetname <2202448@uad.ac.uk>
2022-11-17 17:46:48 +0000

    Changed home page UI

--- C#_Data_Program/WindowsFormsApp1/Form3.Designer.cs
+++ C#_Data_Program/WindowsFormsApp1/Form3.Designer.cs
@@ -35,10 +35,12 @@ namespace WindowsFormsApp1
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -67,7 +69,7 @@ namespace WindowsFormsApp1
             // 
             this.button1.Location = new System.Drawing.Point(6, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 32);
+            this.button1.Size = new System.Drawing.Size(83, 32);
             this.button1.TabIndex = 5;
             this.button1.Text = "View";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,71 +79,91 @@ namespace WindowsFormsApp1
             // 
             this.button2.Location = new System.Drawing.Point(109, 25);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(74, 32);
+            this.button2.Size = new System.Drawing.Size(83, 32);
             this.button2.TabIndex = 6;
             this.button2.Text = "Edit";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(57, 63);
+            this.button3.Location = new System.Drawing.Point(6, 73);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(74, 32);
+            this.button3.Size = new System.Drawing.Size(83, 32);
             this.button3.TabIndex = 7;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button7);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Location = new System.Drawing.Point(38, 164);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.Size = new System.Drawing.Size(210, 173);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Hardware";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(109, 73);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(83, 32);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Retrieve";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button4);
             this.groupBox2.Controls.Add(this.button5);
             this.groupBox2.Controls.Add(this.button6);
-            this.groupBox2.Location = new System.Drawing.Point(559, 164);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Location = new System.Drawing.Point(301, 164);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.Size = new System.Drawing.Size(210, 173);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Software";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(6, 25);
+            this.button4.Location = new System.Drawing.Point(109, 73);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(74, 32);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "View";
+            this.button4.Size = new System.Drawing.Size(83, 32);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Retrieve";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(57, 63);
+            this.button5.Location = new System.Drawing.Point(6, 25);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(74, 32);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "Delete";
+            this.button5.Size = new System.Drawing.Size(83, 32);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "View";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(109, 25);
+            this.button6.Location = new System.Drawing.Point(6, 73);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(74, 32);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "Edit";
+            this.button6.Size = new System.Drawing.Size(83, 32);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Delete";
             this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(109, 25);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(83, 32);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "Edit";
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
@@ -169,9 +191,11 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button7;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
     }
 }
