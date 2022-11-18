a8945d1eeb43399a379f616d54ef16cf6c43fa4f
LukeSweetname <2202448@uad.ac.uk>
2022-11-18 02:57:09 +0000

    Added some functionality to get software button

--- C#_Data_Program/WindowsFormsApp1/Form3.Designer.cs
+++ C#_Data_Program/WindowsFormsApp1/Form3.Designer.cs
@@ -126,6 +126,7 @@ namespace WindowsFormsApp1
             this.button2.TabIndex = 36;
             this.button2.Text = "Check Software";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
--- C#_Data_Program/WindowsFormsApp1/Form3.cs
+++ C#_Data_Program/WindowsFormsApp1/Form3.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Management;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+//using System.Collections;
+
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +19,39 @@ namespace WindowsFormsApp1
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check OS name
+            ManagementClass management1 = new ManagementClass("Win32_OperatingSystem");
+            ManagementObjectCollection managementobject1 = management1.GetInstances();
+
+            foreach (ManagementObject mngObject1 in managementobject1)
+            {
+                textBox1.Text = mngObject1.Properties["Name"].Value.ToString();
+                break;
+            }
+
+            // Check OS version
+            ManagementClass management2 = new ManagementClass("Win32_OperatingSystem");
+            ManagementObjectCollection managementobject2 = management1.GetInstances();
+
+            foreach (ManagementObject mngObject1 in managementobject1)
+            {
+                textBox2.Text = mngObject1.Properties["Name"].Value.ToString();
+                break;
+            }
+
+            // Check OS manufacturer
+            ManagementClass management3 = new ManagementClass("Win32_OperatingSystem");
+            ManagementObjectCollection managementobject3 = management1.GetInstances();
+
+            foreach (ManagementObject mngObject1 in managementobject1)
+            {
+                textBox3.Text = mngObject1.Properties["Name"].Value.ToString();
+                break;
+            }
         }
     }
 }
