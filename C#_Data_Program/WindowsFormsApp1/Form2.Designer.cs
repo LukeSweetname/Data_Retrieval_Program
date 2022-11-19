c233fae12a6ee44f9bd9e6c69816b44314699a9f
LukeSweetname <2202448@uad.ac.uk>
2022-11-19 17:03:53 +0000

    Completed log in functionality and redirection to home page

--- C#_Data_Program/WindowsFormsApp1/Form2.Designer.cs
+++ C#_Data_Program/WindowsFormsApp1/Form2.Designer.cs
@@ -36,6 +36,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -112,11 +114,31 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(547, 266);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(176, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(547, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Error Message";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(753, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
@@ -142,5 +164,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
--- C#_Data_Program/WindowsFormsApp1/Form2.cs
+++ C#_Data_Program/WindowsFormsApp1/Form2.cs
@@ -25,51 +25,14 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string server = "lochnagar.abertay.ac.uk";
-            string database = "sql2202448";
-            string username = "sql2202448";
-            string password = "Fz2ggjKkinH6";
-            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "USERNAME=" + username + ";" + "PASSWORD=" + password + ";";
-
-            MySqlConnection conn = new MySqlConnection(constring);
-            // https://learn.microsoft.com/en-us/troubleshoot/sql/connect/network-related-or-instance-specific-error-occurred-while-establishing-connection
-            // Copyright (c) Microsoft (TM) forums 2022 | Code (C#)
-            // Used the above code to troubleshoot errors with the database connection
-
-            conn.Open();
-
-            string userName;
-            string passWord;
-            
-
-            userName = textBox2.Text;
-            passWord = textBox1.Text;
-            
-
-            // https://stackoverflow.com/questions/22806870/incorrect-datetime-value-database-error-number-1292
-            // Copyright Stack Overflow (c) (TM) 2022 | Code (C#)
-            // Used above link to investigate datetime error and resolve error.
-
-            // https://www.c-sharpcorner.com/article/connect-mysql-with-c-sharp-net-framework-in-visual-studio-2019/
-            // Copyright Ojash Shrestha (c) 2021 | Code (C#)
-
-            // https://www.youtube.com/watch?v=lXAgHdhbEzo
-            // Copyright OpenEducation (c) Youtube (TM) 2021 | Code (C#)
-            // Used the above links as inspiration for my database connection for my program. 
-            MySqlCommand cmd = conn.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "SELECT * FROM user2 WHERE userName = '" + textBox2.Text + "' AND passWord = '" + textBox1.Text + "'";
+            MySqlConnection con = new MySqlConnection("server= lochnagar.abertay.ac.uk; database= sql2202448; username = sql2202448; password = Fz2ggjKkinH6");
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand("select * from user2 where userName = '" + textBox2.Text + "' AND passWord = '" + textBox1.Text + "'", con);
             MySqlDataReader reader = cmd.ExecuteReader();
 
-            textBox1.Text = string.Empty;
-            textBox2.Text = string.Empty;
-
-            reader.Close();
-            cmd.Dispose();
-
-            if ((userName == textBox2.Text) & (passWord == textBox1.Text))
+            if (reader.Read())
             {
-                Form2 formname = new Form2();
+                Form4 formname = new Form4();
                 formname.Show();
                 textBox2.Text = "";
                 textBox1.Text = "";
@@ -77,9 +40,11 @@ namespace WindowsFormsApp1
             {
                 textBox2.Text = "";
                 textBox1.Text = "";
-                Console.WriteLine("The details you have entered are incorrect, please try again.");
+                richTextBox1.Text = "The details you have entered are incorrect, please try again.";
             }
-            conn.Close(); // close the connection
+            reader.Close();
+            cmd.Dispose();
+            con.Close(); // close the connection
             Console.WriteLine("\nConnection successfully terminated.");
         }
 
@@ -101,6 +66,11 @@ namespace WindowsFormsApp1
                 // Cancel closing form
                 Application.Exit();
             }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
     }
