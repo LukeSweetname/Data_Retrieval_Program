2030d440f0298dbd9a9de0d3bc79d07ac402160d
LukeSweetname <2202448@uad.ac.uk>
2022-11-20 22:59:07 +0000

    Continued working on hardware edit/delete functionality

--- C#_Data_Program/WindowsFormsApp1/Form5.cs
+++ C#_Data_Program/WindowsFormsApp1/Form5.cs
@@ -18,106 +18,51 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        string connectionString = "server=lochnagar.abertay.ac.uk;user id=sql2202448;password=Fz2ggjKkinH6;database=sql2202448";
+
         private void button2_Click(object sender, EventArgs e)
         {
-            string server = "lochnagar.abertay.ac.uk";
-            string database = "sql2202448";
-            string username = "sql2202448";
-            string password = "Fz2ggjKkinH6";
-            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "USERNAME=" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection con = new MySqlConnection(connectionString);
 
-            MySqlConnection conn = new MySqlConnection(constring);
-            // https://learn.microsoft.com/en-us/troubleshoot/sql/connect/network-related-or-instance-specific-error-occurred-while-establishing-connection
-            // Copyright (c) Microsoft (TM) forums 2022 | Code (C#)
-            // Used the above code to troubleshoot errors with the database connection
-
-            conn.Open();
-
-            string systemName;
-            string systemModel;
-            string systemManufacturer;
-            string systemType;
-            string systemIPaddress;
-            string systemPurchaseDate;
-            string systemExtraDetails;
-
-            systemName = textBox1.Text;
-            systemModel = textBox2.Text;
-            systemManufacturer = textBox3.Text;
-            systemType = textBox4.Text;
-            systemIPaddress = textBox5.Text;
-            systemPurchaseDate = dateTimePicker1.Text;
-            systemExtraDetails = textBox7.Text;
-
-            // https://stackoverflow.com/questions/22806870/incorrect-datetime-value-database-error-number-1292
-            // Copyright Stack Overflow (c) (TM) 2022 | Code (C#)
-            // Used above link to investigate datetime error and resolve error.
-
-            // https://www.c-sharpcorner.com/article/connect-mysql-with-c-sharp-net-framework-in-visual-studio-2019/
-            // Copyright Ojash Shrestha (c) 2021 | Code (C#)
-
-            // https://www.youtube.com/watch?v=lXAgHdhbEzo
-            // Copyright OpenEducation (c) Youtube (TM) 2021 | Code (C#)
-            // Used the above links as inspiration for my database connection for my program. 
-            MySqlConnection con = new MySqlConnection("server= lochnagar.abertay.ac.uk; database= sql2202448; username = sql2202448; password = Fz2ggjKkinH6");
             con.Open();
-            MySqlCommand cmd = new MySqlCommand("select * from hardwareData where systemName = @systemName", conn);
+
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * FROM hardwareData where systemName = @systemName";
+            cmd.Parameters.AddWithValue("@systemName", textBox1.Text);
+            cmd.Parameters.AddWithValue("@systemName", textBox1.Text);
+            cmd.Parameters.AddWithValue("@systemName", textBox1.Text);
             MySqlDataReader reader = cmd.ExecuteReader();
-
-            conn.Close(); // close the connection
-            Console.WriteLine("\nConnection successfully terminated.");
-
+            while (reader.Read())
+            {
+                MessageBox.Show(reader["hardwareData"].ToString());
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string server = "lochnagar.abertay.ac.uk";
-            string database = "sql2202448";
-            string username = "sql2202448";
-            string password = "Fz2ggjKkinH6";
-            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "USERNAME=" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection con = new MySqlConnection(connectionString);
 
-            MySqlConnection conn = new MySqlConnection(constring);
-            // https://learn.microsoft.com/en-us/troubleshoot/sql/connect/network-related-or-instance-specific-error-occurred-while-establishing-connection
-            // Copyright (c) Microsoft (TM) forums 2022 | Code (C#)
-            // Used the above code to troubleshoot errors with the database connection
+            con.Open();
 
-            conn.Open();
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.Parameters.AddWithValue("@systemName", textBox1.Text);
+            cmd.Parameters.AddWithValue("@systemModel", textBox2.Text);
+            cmd.Parameters.AddWithValue("@systemManufacturer", textBox3.Text);
+            cmd.Parameters.AddWithValue("@systemType", textBox4.Text);
+            cmd.Parameters.AddWithValue("@systemIPaddress", textBox5.Text);
+            cmd.Parameters.AddWithValue("@systemPurchaseDate", dateTimePicker1.Text);
+            cmd.Parameters.AddWithValue("@systemMACaddress", textBox6.Text);
+            cmd.Parameters.AddWithValue("@systemExtraDetails", textBox7.Text);
 
-            string systemName;
-            string systemModel;
-            string systemManufacturer;
-            string systemType;
-            string systemIPaddress;
-            string systemPurchaseDate;
-            string systemExtraDetails;
+            cmd.CommandText = "INSERT INTO hardwareData (systemName, systemModel, systemManufacturer, systemType, systemIPaddress, systemPurchaseDate, systemMACaddress, systemExtraDetails)" +
+                "VALUES (@systemName, @systemModel, @systemManufacturer, @systemType, @systemIPaddress, @systemPurchaseDate, @systemMACaddress, @systemExtraDetails)";
 
-            systemName = textBox1.Text;
-            systemModel = textBox2.Text;
-            systemManufacturer = textBox3.Text;
-            systemType = textBox4.Text;
-            systemIPaddress = textBox5.Text;
-            systemPurchaseDate = dateTimePicker1.Text;
-            systemExtraDetails = textBox7.Text;
+            if (cmd.ExecuteNonQuery() > 0)
+                MessageBox.Show("Data was added to database");
+            else
+                MessageBox.Show("Data was not added to database");
+            con.Close();
 
-            // https://stackoverflow.com/questions/22806870/incorrect-datetime-value-database-error-number-1292
-            // Copyright Stack Overflow (c) (TM) 2022 | Code (C#)
-            // Used above link to investigate datetime error and resolve error.
-
-            // https://www.c-sharpcorner.com/article/connect-mysql-with-c-sharp-net-framework-in-visual-studio-2019/
-            // Copyright Ojash Shrestha (c) 2021 | Code (C#)
-
-            // https://www.youtube.com/watch?v=lXAgHdhbEzo
-            // Copyright OpenEducation (c) Youtube (TM) 2021 | Code (C#)
-            // Used the above links as inspiration for my database connection for my program. 
-            MySqlCommand cmd = conn.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "INSERT INTO hardwareData (systemName, systemModel, systemManufacturer, systemType, systemIPaddress, systemPurchaseDate, systemExtraDetails)" +
-                "VALUES ('" + systemName + "','" + systemModel + "','" + systemManufacturer + "','" + systemType + "','" + systemIPaddress + "','" + systemPurchaseDate + "','" + systemExtraDetails + "')";
-            cmd.ExecuteNonQuery();
-
-            conn.Close(); // close the connection
-            Console.WriteLine("\nConnection successfully terminated.");
 
         }
     }
