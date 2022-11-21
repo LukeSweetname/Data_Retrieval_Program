48b04a24ef9c6758f28b370e9a2c57c7ccb861d4
LukeSweetname <2202448@uad.ac.uk>
2022-11-21 15:46:52 +0000

    removed exit button

--- C#_Data_Program/WindowsFormsApp1/Form5.Designer.cs
+++ C#_Data_Program/WindowsFormsApp1/Form5.Designer.cs
@@ -31,7 +31,6 @@ namespace WindowsFormsApp1
         {
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -57,16 +56,6 @@ namespace WindowsFormsApp1
             this.button5.TabIndex = 54;
             this.button5.Text = "Clear Form";
             this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(1118, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(88, 38);
-            this.button3.TabIndex = 48;
-            this.button3.Text = "Exit";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
@@ -127,7 +116,6 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -143,7 +131,6 @@ namespace WindowsFormsApp1
 
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
