ad810cef5306599b6bbef7f4639771f54aeb81a6
LukeSweetname <2202448@uad.ac.uk>
2022-11-21 18:47:58 +0000

    Created software edit/delete

--- C#_Data_Program/WindowsFormsApp1/Form6.Designer.cs
+++ C#_Data_Program/WindowsFormsApp1/Form6.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class Form5
+    partial class Form6
     {
         /// <summary>
         /// Required designer variable.
@@ -29,31 +29,52 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-1, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1406, 20);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "To view the data stored on the database, simply click on \'Retrieve from Database\'" +
+    ". You can then double click on any data entry to edit or delete the data. Once c" +
+    "omplete, click \'Send back to Database\'.";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1255, 278);
+            this.dataGridView1.TabIndex = 66;
+            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(1105, 386);
+            this.button4.Location = new System.Drawing.Point(1102, 384);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(166, 52);
-            this.button4.TabIndex = 55;
+            this.button4.TabIndex = 65;
             this.button4.Text = "Return to Menu";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(985, 386);
+            this.button5.Location = new System.Drawing.Point(982, 384);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(114, 52);
-            this.button5.TabIndex = 54;
+            this.button5.TabIndex = 64;
             this.button5.Text = "Clear Form";
             this.button5.UseVisualStyleBackColor = true;
             // 
@@ -61,60 +82,35 @@ namespace WindowsFormsApp1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(416, 11);
+            this.label1.Location = new System.Drawing.Point(431, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(435, 46);
-            this.label1.TabIndex = 47;
+            this.label1.TabIndex = 63;
             this.label1.Text = "Data Retrieval Program";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(218, 386);
+            this.button2.Location = new System.Drawing.Point(215, 384);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(219, 52);
-            this.button2.TabIndex = 37;
+            this.button2.TabIndex = 62;
             this.button2.Text = "Retrieve from Database";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 386);
+            this.button1.Location = new System.Drawing.Point(13, 384);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(196, 52);
-            this.button1.TabIndex = 36;
+            this.button1.TabIndex = 61;
             this.button1.Text = "Send back to Database\r\n";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 78);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1255, 278);
-            this.dataGridView1.TabIndex = 59;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 363);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1406, 20);
-            this.label2.TabIndex = 60;
-            this.label2.Text = "To view the data stored on the database, simply click on \'Retrieve from Database\'" +
-    ". You can then double click on any data entry to edit or delete the data. Once c" +
-    "omplete, click \'Send back to Database\'.";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // Form5
+            // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1296, 450);
+            this.ClientSize = new System.Drawing.Size(1280, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
@@ -122,8 +118,8 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form5";
-            this.Text = "Form5";
+            this.Name = "Form6";
+            this.Text = "Form6";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -132,12 +128,12 @@ namespace WindowsFormsApp1
 
         #endregion
 
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label2;
     }
 }
