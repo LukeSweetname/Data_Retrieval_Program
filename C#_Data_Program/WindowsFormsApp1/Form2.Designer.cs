2a7edba05fd5c3fc0ae6392179940a276b275bf4
LukeSweetname <2202448@uad.ac.uk>
2022-11-28 22:13:18 +0000

    Added instructions to form2

--- C#_Data_Program/WindowsFormsApp1/Form2.Designer.cs
+++ C#_Data_Program/WindowsFormsApp1/Form2.Designer.cs
@@ -39,6 +39,7 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // passwordField
@@ -143,11 +144,22 @@
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(259, 160);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Please provide a valid username \r\nand password, then click \'Sign in\'.\r\n\r\n\r\n\r\n\r\nUs" +
+    "e the \'Show Password\' checkbox\r\nto view the chracters entered.\r\n";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(753, 450);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.richTextBox1);
@@ -182,5 +194,6 @@
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
