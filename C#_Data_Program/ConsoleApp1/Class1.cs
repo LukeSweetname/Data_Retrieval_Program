25aaf5983ccbeef777ae07b905dc9f955e3c90de
LukeSweetname <2202448@uad.ac.uk>
2022-12-03 22:51:18 +0000

    api work

---
+++ C#_Data_Program/ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class NistModel
+    {
+        public string id { get; set; }
+        // id
+        public string cisaRequiredAction { get; set; }
+        // cisarequiredaction
+        public decimal value { get; set; }
+        // description (value)
+        public string accessComplexity { get; set; }
+        // accesscomplexity
+        public string baseSeverity { get; set; }
+        // baseseverity
+        public string exploitabilityScore { get; set; }
+        // exploitabilityscore
+        public string impactScore { get; set; }
+        // impactscore
+    }
+}
--- C#_Data_Program/ConsoleApp1/Program.cs
+++ C#_Data_Program/ConsoleApp1/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace WebApiClient
 {
     class Program
     {
-        static void Main(string[] args)
+
+        /*
+        HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            await program.GetTodoItems();
+        }
+
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync(
+                "https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName=cpe:2.3:o:microsoft:windows_10:-:id:*:*:*:*:*:*");
+
+            Console.WriteLine(response);
+            if (response.IsSuccessStatusCode)
+            {
+
+            }
+        }
+        */
+        public class Product
+        {
+            public string id { get; set; }
+            // id
+            public string cisaRequiredAction { get; set; }
+            // cisarequiredaction
+            public decimal value { get; set; }
+            // description (value)
+            public string accessComplexity { get; set; }
+            // accesscomplexity
+            public string baseSeverity { get; set; }
+            // baseseverity
+            public string exploitabilityScore { get; set; }
+            // exploitabilityscore
+            public string impactScore { get; set; }
+            // impactscore
+        }
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("press any key to continue");
+            Console.ReadLine();
+
+            
+            using (System.Net.Http.HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName=cpe:2.3:o:microsoft:windows_10:-:id:*:*:*:*:*:*");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    Console.WriteLine($"response error code: {response.StatusCode}");
+                }
+                
+            }
+
         }
     }
 }
