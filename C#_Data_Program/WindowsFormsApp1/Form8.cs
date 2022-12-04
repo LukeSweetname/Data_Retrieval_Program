4a4598426f1a06d6ba61431fd76996789ff5d419
LukeSweetname <2202448@uad.ac.uk>
2022-12-04 17:51:29 +0000

    created datagridview and new form for output of api

---
+++ C#_Data_Program/WindowsFormsApp1/Form8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://services.nvd.nist.gov/rest/json/cves/2.0?");
+            // cpeName=cpe:2.3:o:microsoft:windows_10:-:*:*:*:*:*:*:*
+            HttpResponseMessage response = client.GetAsync("cpeName=cpe:2.3:o:microsoft:windows_10:-:*:*:*:*:*:*:*").Result;
+            var vun = response.Content.ReadAsAsync<IEnumerable<vunerabilities>>().Result;
+            dataGridView1.DataSource = vun;
+        }
+    }
+}
---
+++ C#_Data_Program/WindowsFormsApp1/vunerabilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class vunerabilities
+    {
+        public string id { get; set; }
+        // id
+        public string cisaRequiredAction { get; set; }
+        // cisarequiredaction
+        public decimal value { get; set; }
+        // description (value)
+        public string accessComplexity { get; set; }
+        // accesscomplexity
+        public string baseSeverity { get; set; }
+        // baseseverity
+        public string exploitabilityScore { get; set; }
+        // exploitabilityscore
+        public string impactScore { get; set; }
+        // impactscore
+    }
+}
